dc1846d64fa2f5463af4855c16b50c67f648c7e4
Diego Casavilla <diegocasavilla@gmail.com>
2021-11-05 22:54:24 -0300

    Creando archivos

---
+++ integrador.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Ejercicio integrador creado por Mikiztly (Diego Casavilla)
+
+Alumnos = {}
+
+
+    Ingrese el número de la operación que desea ejecutar:
+    1 - Añadir un alumno a la lista.
+    2 - Ver la lista de alumnos.
+    3 - Salir
+    >>>
