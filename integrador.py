d2a5b6016eea5725f55b5169fcdb37d17c713975
Mikiztly <diegocasavilla@gmail.com>
2021-11-14 21:14:03 -0300

    Agregados los comentarios para aclarar el codigo

--- integrador.py
+++ integrador.py
@@ -2,12 +2,17 @@
 # -*- coding: utf-8 -*-
 # Ejercicio integrador creado por Mikiztly (Diego Casavilla)
 
+# Primero inicializo la lista
 Alumnos = []
 
+# Encabezado del menu
 print("Administración de alumnos")
 print("-------------------------")
 
+# Comienzo el bucle hasta que el usuario quiera salir
 while True:
+    
+    # Imprimo el menu
     print(
     """
         Menú de opciones
@@ -19,13 +24,16 @@ while True:
         -----------------------------------------------------
     """)
 
+    # Le doy la entrada para que el usuario ingrese una opcion
     Opcion = input("Seleccione una opción: ")
 
+    # Segun la opcion es la accion que debo hacer
+    # Primero es para agregar alumos
     if Opcion == "1":
         Nombre = input("Ingrese el nombre del nuevo alumno: ")
         Cursos = input("Ingrse la cantidad de cuesos que tiene: ")
         Alumnos.append([Nombre,Cursos])
-
+    # Segundo listar alumnos
     elif Opcion == "2":
         if not Alumnos:
             print("No hay alumnos")
@@ -36,9 +44,10 @@ while True:
                     print(f"{XLoop[0]} tiene {XLoop[1]} cursos")
                 else:
                     print(f"{XLoop[0]} tiene {XLoop[1]} curso")
-
+    # Tercero es para salir
     elif Opcion == "3":
         print("Gracias por usar este programa...")
         break
+    # Si pone cualquier cosa le digo que es incorrecto
     else:
         print("Opción incorrecta...")
